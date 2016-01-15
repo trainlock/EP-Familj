78aea0f00a964470b339f1901c9ef77c74bb9a8a
Vilhelm Engström <vilen280@student.liu.se>
2016-01-15 14:58:40 +0100

    Kommenterat koden

--- helpAnimation.js
+++ helpAnimation.js
@@ -1,3 +1,14 @@
+/* Tanken bakom animationen är att skapa en mindre version av hemsidan, 
+med andra ord genereras alla element, all text osv. via javascript i den animationsdiv
+som finns i help.html. All rörelse är gjord via att sätta intervaller på ett liknande
+sätt som den klocka vi gjorde under en av laborationerna, nämligen genom att kalla en funktion upprepade
+gånger och för varje gång göra en koll om elementet har nått sin önskade position.
+Det finns även en del funktioner för att hantera för små fönsterstorlekar och för att kunna avbryta 
+uppspelningen på ett korrekt sätt.
+ */
+
+
+
 const demoHeight = 400; //px
 const demoWidth = 600; //px
 const moveMouseAt = 1; //px per ms
